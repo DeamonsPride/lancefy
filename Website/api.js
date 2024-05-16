7f37223451f0c5ac088b825ceadc595edb526893
Snowy <jamie.marjanic@gmail.com>
2024-05-17 00:20:41 +0200

    Updated dashboard

---
+++ Website/api.js
@@ -0,0 +1,272 @@
+var device = '';
+function station(name) {
+    if (name === 'avenijazaggzc01') {
+        device = '41264160-fb47-11ee-9cc4-f58d0edc07e6';
+    } else if (name === 'helenasmbzgc01') {
+        device = 'placeholder'; // Reset device to empty string for other cases
+    }
+    console.log('Device set to:', device);
+}
+
+console.log('Device set to:', device);
+
+function fetchDataForDevice(device) {
+    console.log('Fetching data for device:', device);
+    if (device) {
+// AUTHENTICATION FETCH
+fetch('http://193.122.14.71:8085/api/auth/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        "username": "admin@esclone.com", 
+        "password": "82aTiLGM&"
+    }),
+    mode: 'cors',
+    cache: 'default',
+})
+    .then(res => res.json())
+    .then(data => {
+        console.log('Authentication response:', data);
+        const token = data.token;
+        //month
+        const endTs = Date.now();
+        const startTs = endTs - (30 * 24 * 60 * 60 * 1000);
+        //day
+        const DendTs = Date.now();
+        const DstartTs = endTs - (24 * 60 * 60 * 1000);
+
+
+        //TEMPERATURE FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=temperature`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const temperatureValue = data.temperature[0].value; // Access the temperature value from the response
+                document.getElementById("temp").innerHTML = temperatureValue;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+
+        //PRESSURE FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=pressure`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const pressureValue = data.pressure[0].value;
+                document.getElementById("press").innerHTML = pressureValue;
+                updatePressureGraph(pressureValue);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+
+        //LUX FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=luxLevel`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const luxValue = data.luxLevel[0].value;
+                document.getElementById("lux").innerHTML = luxValue;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+        //HISTORICAL LUX FETCH
+        fetch(`https://dash.esclone.com:443/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=luxLevel&startTs=${DstartTs}&endTs=${DendTs}&intervalType=MILLISECONDS&interval=3600000&timeZone=GMT%2B02%3A00&limit=100&agg=AVG&orderBy=ASC`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                updateLuxGraph(data.luxLevel);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+
+        //HUMIDITY FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=humidity`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const humidityValue = data.humidity[0].value;
+            document.getElementById("hum").innerHTML = humidityValue;
+            updateHumidityGraph(humidityValue);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+
+
+        //UV INDEX FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=uvIndex`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const uvValue = data.uvIndex[0].value;
+                document.getElementById("uv").innerHTML = uvValue;
+                updateUvGraph(uvValue);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+
+        //HEAT INDEX FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=heatIndex`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const heatValue = data.heatIndex[0].value;
+                document.getElementById("heat").innerHTML = heatValue;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+
+        //ABSOLUTE HUMIDITY FETCH
+        fetch(`http://193.122.14.71:8085/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=absHum`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const ahumValue = data.absHum[0].value;
+                document.getElementById("abhum").innerHTML = ahumValue;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+        //HISTORICAL ABSOLUTE HUMIDITY FETCH
+        fetch(`https://dash.esclone.com:443/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=absHum&startTs=${startTs}&endTs=${endTs}&intervalType=MILLISECONDS&interval=86400000&timeZone=GMT%2B02%3A00&limit=100&agg=AVG&orderBy=ASC`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                updateAbHumidityGraph(data.absHum);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+
+        //HISTORICAL TEMPERATURE/HUMIDITY FETCH
+        function processApiResponse(apiResponse, key) {
+            return apiResponse[key].map(item => ({
+                date: new Date(item.ts).toLocaleDateString(),
+                value: item.value
+            }));
+        }
+        const humidityMinFetch = fetch(`https://dash.esclone.com:443/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=humidity&startTs=${startTs}&endTs=${endTs}&interval=86400000&agg=MIN&limit=30&orderBy=ASC`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        });
+
+        const humidityMaxFetch = fetch(`https://dash.esclone.com:443/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=humidity&startTs=${startTs}&endTs=${endTs}&interval=86400000&agg=MAX&limit=30&orderBy=ASC`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        });
+
+        const temperatureAvgFetch = fetch(`https://dash.esclone.com:443/api/plugins/telemetry/DEVICE/${device}/values/timeseries?keys=temperature&startTs=${startTs}&endTs=${endTs}&interval=86400000&agg=AVG&limit=30&orderBy=ASC`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default',
+        });
+
+        Promise.all([humidityMinFetch, humidityMaxFetch, temperatureAvgFetch])
+            .then(responses => Promise.all(responses.map(res => res.json())))
+            .then(([minHumidityData, maxHumidityData, avgTemperatureData]) => {
+                const minHumidity = processApiResponse(minHumidityData, 'humidity');
+                const maxHumidity = processApiResponse(maxHumidityData, 'humidity');
+                const medianTemperature = processApiResponse(avgTemperatureData, 'temperature');
+
+                const minHumidityProcessed = { dates: minHumidity.map(d => d.date), values: minHumidity.map(d => d.value) };
+                const maxHumidityProcessed = { dates: maxHumidity.map(d => d.date), values: maxHumidity.map(d => d.value) };
+                const medianTemperatureProcessed = { dates: medianTemperature.map(d => d.date), values: medianTemperature.map(d => d.value) };
+
+                updateHumidityAndTemperatureGraph(minHumidityProcessed, maxHumidityProcessed, medianTemperatureProcessed);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    })
+    .catch(error => console.error('Error fetching token:', error));
+} 
+}
+// Function to update the device variable
+function updateDevice(newDevice) {
+    device = newDevice;
+    console.log('Device set to:', device);
+    fetchDataForDevice(device); // Call fetch function whenever device is updated
+}  
